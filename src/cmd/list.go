aa62aed003ecaeba6b7033737daa66b8d8f57ebd
tydunn <t.dunn@rasa.com>
2022-07-20 14:07:48 +0200

    next iteration on list CLI

--- src/cmd/list.go
+++ src/cmd/list.go
@@ -14,35 +14,61 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "View current metadata",
 	Long: `View current metadata, including
-	- sources
 	- permissions
 	- royalties
 	- schemas
-	- cache
 	- wallet
 	as a consumer or producer.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
-		mode, _ := cmd.Flags().GetString("mode")
-		entity, _ := cmd.Flags().GetString("entity")
+		op, _ := cmd.Flags().GetString("op")
+		metadata, _ := cmd.Flags().GetString("metadata")
 
-		switch mode {
+		method := "LevelDbHttpAdapter.Read(%s)\n"
+
+		switch op {
 		case "consumer":
-			switch entity {
-			case "sources":
-				fmt.Println("list in consumer mode called with sources entity")
+			switch metadata {
+			case "permissions":
+				url := "basin://local.consumer.permissions.metadata"
+				fmt.Printf(method, url)
+			case "requests":
+				url := "basin://local.consumer.requests.metadata"
+				fmt.Printf(method, url)
+			case "royalties":
+				url := "basin://local.consumer.royalties.metadata"
+				fmt.Printf(method, url)
+			case "schemas":
+				url := "basin://local.consumer.schemas.metadata"
+				fmt.Printf(method, url)
+			case "wallet":
+				url := "basin://local.consumer.wallet.metadata"
+				fmt.Printf(method, url)
 			default:
-				fmt.Println("error: list in consumer mode must be called with sources entity")
+				fmt.Println("error: list in consumer mode must be called with an entity")
 			}
 		case "producer":
-			switch entity {
-			case "sources":
-				fmt.Println("list in producer mode called with sources entity")
+			switch metadata {
+			case "permissions":
+				url := "basin://local.producer.permissions.metadata"
+				fmt.Printf(method, url)
+			case "requests":
+				url := "basin://local.producer.requests.metadata"
+				fmt.Printf(method, url)
+			case "royalties":
+				url := "basin://local.producer.royalties.metadata"
+				fmt.Printf(method, url)
+			case "schemas":
+				url := "basin://local.producer.schemas.metadata"
+				fmt.Printf(method, url)
+			case "wallet":
+				url := "basin://local.producer.wallet.metadata"
+				fmt.Printf(method, url)
 			default:
-				fmt.Println("error: list in producer mode must be called with sources entity")
+				fmt.Println("error: list in producer mode must be called with an entity")
 			}
 		default:
-			fmt.Println("error: list must be called in either consumer or producer mode with sources entity.")
+			fmt.Println("error: list must be called in either consumer or producer mode with an entity.")
 		}
 
 	},
@@ -50,10 +76,10 @@ var listCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(listCmd)
-	listCmd.PersistentFlags().StringP("mode", "m", "", "consumer or producer mode")
-	listCmd.MarkPersistentFlagRequired("mode")
-	listCmd.PersistentFlags().StringP("entity", "e", "", "entities (e.g. sources)")
-	listCmd.MarkPersistentFlagRequired("entity")
+	listCmd.PersistentFlags().StringP("op", "o", "", "consumer or producer mode")
+	listCmd.MarkPersistentFlagRequired("op")
+	listCmd.PersistentFlags().StringP("metadata", "m", "", "metadata (e.g. permissions)")
+	listCmd.MarkPersistentFlagRequired("metadata")
 	// https://github.com/spf13/cobra/blob/main/user_guide.md#flag-groups
 	// rootCmd.MarkFlagsRequiredTogether("username", "password")
 }
