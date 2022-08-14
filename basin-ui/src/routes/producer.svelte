c949ae748f1b5791829827d546640f1ba6aec6d7
sestinj <sestinj@gmail.com>
2022-08-13 22:00:15 -0700

    File and concept for basin gen command, which can generate typescript types for developers

---
+++ basin-ui/src/routes/producer.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let resources: ProducerResource;
+</script>
+
+<h2>Resources</h2>
+
+<table>
+    <thead>
+        <tr>
+            <th>URL</th>
+            <th>Health</th>
+            <th>Revenue</th>
+            <th>Subscribers</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each resources as resource}
+            
+        {/each}
+    </tbody>
+</table>
