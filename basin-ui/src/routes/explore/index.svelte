dd4dca7c5238087234d136a5f0e49ca7d5e404d2
sestinj <sestinj@gmail.com>
2022-08-15 22:01:39 -0700

    Messing around with explore page cards

--- basin-ui/src/routes/explore/index.svelte
+++ basin-ui/src/routes/explore/index.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
+    import Card from "@lib/Card.svelte";
+    import Basin from "@util/basin";
+    // Really want to be getting this list of resources from a curating transformer, but this is a demo
+    let featuredResources = [
+        "tydunn.com.twitter.followers",
+        "tydunn.com.twitter.following"
+    ];
+
+    // Get info like icon, description, title, etc... from the sources.
 
 </script>
 
-<h2 class="text-2xl">Popular Transformers</h2>
+<div class="grid grid-cols-12 gap-4">
+    {#each featuredResources as resourceUrl}
+        <Card className="col-span-2 w-full" title={resourceUrl}>
+            
+        </Card>
+    {/each}
+</div>
+
+<!-- <h2 class="text-2xl">Popular Transformers</h2>
 
 <h2 class="text-2xl">Popular Schemas</h2>
 
-<h2 class="text-2xl">Popular Datasets</h2>
+<h2 class="text-2xl">Popular Datasets</h2> -->
+
